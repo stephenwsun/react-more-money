163aa6ef443ddc61564557b4327084a70d353326
Stephen Sun <stephensun.me@gmail.com>
2020-04-26 11:32:31 -0500

    integrated accounts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState, useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import { usePlaidLink } from 'react-plaid-link'
-import { Layout, Menu, Button } from 'antd'
+import { Button, Layout, Menu } from 'antd'
 import {
   DesktopOutlined,
   PieChartOutlined,
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons'
+import Dashboard from './screens/Dashboard'
 import './App.css'
 
 const { Header, Content, Footer, Sider } = Layout
@@ -14,22 +16,21 @@ const { SubMenu } = Menu
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(false)
+  const [accounts, setAccounts] = useState([])
+  const [transactions, setTransactions] = useState([])
 
-  const onCollapse = collapsed => {
-    setCollapsed(collapsed)
-  }
-
-  const onSuccess = useCallback(async (token, metadata) => {
-    const tokenData = await fetch('http://localhost:8000/auth/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ public_token: token }),
+  useEffect(() => {
+    getAllTransactions().then(data => {
+      const { accounts, transactions } = data.transactions
+      setAccounts(accounts)
+      setTransactions(transactions)
     })
+  }, [])
 
-    const tokenJSON = await tokenData.json()
-    const accessToken = tokenJSON.access_token
-
-    const transactionData = await fetch('http://localhost:8000/transactions', {
+  const getAllTransactions = async () => {
+    // const accessToken = await getAccessToken(token)
+    const accessToken = 'access-sandbox-863f6ead-5aac-45bd-a5be-d609778479e9'
+    const res = await fetch('http://localhost:8000/transactions', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -37,8 +38,58 @@ const App = () => {
       },
     })
 
-    const transactionJSON = await transactionData.json()
-    console.log(`Transactions: ${JSON.stringify(transactionJSON)}`)
+    if (200 !== res.status) {
+      console.error('Error getting transactions')
+    }
+
+    return await res.json()
+  }
+
+  const getAccessToken = async token => {
+    const res = await fetch('http://localhost:8000/auth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ public_token: token }),
+    })
+
+    if (200 !== res.status) {
+      console.error('Error getting access token')
+    }
+
+    const json = await res.json()
+    return json.access_token
+  }
+
+  const renderAccounts = () => {
+    return (
+      <SubMenu
+        key="sub1"
+        title={
+          <span>
+            <UserOutlined />
+            <span>Accounts</span>
+          </span>
+        }
+      >
+        {Array.isArray(accounts) && accounts.length > 0 ? (
+          accounts.map(account => <Menu.Item>{account.name}: {currency.format(account.balances.current)}</Menu.Item>)
+        ) : null}
+      </SubMenu>
+    )
+  }
+
+  const currency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
+
+  const onSuccess = useCallback(async (token, metadata) => {
+    const data = await getAllTransactions(token)
+    const { accounts, transactions } = data.transactions
+
+    setAccounts(accounts)
+    setTransactions(transactions)
   }, [])
 
   const config = {
@@ -47,68 +98,59 @@ const App = () => {
     product: ['auth', 'transactions'],
     publicKey: 'aa41d2677d293ac2352aee21491d61',
     onSuccess,
-    // ...
+  }
+
+  const onCollapse = collapsed => {
+    setCollapsed(collapsed)
   }
 
   const { open, ready, error } = usePlaidLink(config)
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1">
-            <PieChartOutlined />
-            <span>Home</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <DesktopOutlined />
-            <span>Budget</span>
-          </Menu.Item>
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <UserOutlined />
-                <span>Transactions</span>
-              </span>
-            }
-          >
-            <Menu.Item key="3">View Transactions</Menu.Item>
-            <Menu.Item key="4">Add Transaction</Menu.Item>
-            <Menu.Item key="5">Delete Transaction</Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="sub2"
-            title={
-              <span>
-                <TeamOutlined />
-                <span>Budget</span>
-              </span>
-            }
-          >
-            <Menu.Item key="6">To Be Budgeted</Menu.Item>
-            <Menu.Item key="8">Expenses</Menu.Item>
-          </SubMenu>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
-          >
-            <Button type="primary" onClick={() => open()} disabled={!ready}>
-              Connect a bank account
-            </Button>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          More Money ©2020 Created by Stephen Sun
-        </Footer>
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <h1 className="logo">More Money</h1>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <PieChartOutlined />
+              <span>Dashboard</span>
+            </Menu.Item>
+            {renderAccounts()}
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <TeamOutlined />
+                  <span>Budget</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">To Be Budgeted</Menu.Item>
+              <Menu.Item key="8">Expenses</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content style={{ margin: '0 16px' }}>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Button type="primary" onClick={() => open()} disabled={!ready}>
+                Connect a bank account
+              </Button>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            More Money ©2020 Created by Stephen Sun
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   )
 }
 export default App
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'antd'
+
+const Dashboard = () => {
+  return (
+    <Row>
+      <Col span={24}>
+        Dashboard Page
+      </Col>
+    </Row>
+  )
+}
+
+export default Dashboard
